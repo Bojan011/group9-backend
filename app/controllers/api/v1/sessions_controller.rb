e167b14eac50beda428cbaba51d8a880bc7c50ca
Bojan Djordjevic <bdjordje@ryerson.ca>
2013-03-19 14:56:32 -0400

    changed login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < Devise::SessionsController
       format.html { super }  
       format.json {  
         warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")  
-        render :status => 200, :json => { :error => "Success" }  
+        render :status => 200, :json => { :success => true, :error => "Success" }  
       }  
     end  
   end  

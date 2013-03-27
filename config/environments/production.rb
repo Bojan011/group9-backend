6464374b63e20b720220d51023f00b6ca83d0f7a
Bojan Djordjevic <bdjordje@ryerson.ca>
2013-03-26 20:40:26 -0400

    Changed host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Group9::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.default_url_options = { :host => 'http://cryptic-hollows-1268.herokuapp.com/admin' }
   # Code is not reloaded between requests
   config.cache_classes = true
 

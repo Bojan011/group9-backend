bc997346df55aa1183abc6918c349f146e690397
Sasi kumar <sasi.madhav2005@gmail.com>
2013-04-11 00:02:23 +0530

    ram | fixing skip authenticity filter

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,6 @@
-class Api::V1::SessionsController < Devise::SessionsController  
+class Api::V1::SessionsController < Devise::SessionsController 
+   skip_before_filter :verify_authenticity_token,
+                     :if => Proc.new { |c| c.request.format == 'application/json' }
   def create  
     respond_to do |format|  
       format.html { super }  

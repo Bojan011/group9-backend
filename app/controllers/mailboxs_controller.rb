f989fd6eb05e4fc8ca3f274ee149f23eed93f309
gentle0219 <gentle0219@hotmail.com>
2013-04-13 10:05:04 +0800

    modify mailbox controller

--- app/controllers/mailboxs_controller.rb
+++ app/controllers/mailboxs_controller.rb
@@ -1,10 +1,16 @@
 class MailboxsController < ApplicationController	
+
   def inbox
-    @mail_box = current_user.mailbox.inbox
+  	current_user = User.find_by_authentication_token(params[:token])
+    @mail_box = current_user.mailbox.inbox if current_user.present?
     
     if @mail_box.present?
 	    inbox = @mail_box.map do |mail|
-	    	{:id=>mail.id, :created_at => mail.created_at, :is_unread=> is_unread? mail.receipts, :sender=> mail.last_message.sender.name }
+	    	id = mail.id
+	    	created_at = mail.created_at
+	    	is_unread = unread?(mail)
+	    	sender = mail.last_message.sender.name 
+	    	{:id=>id, :created_at => created_at, :is_unread=> is_unread, :sender=> sender}
 	    end
 	    json = inbox.to_json 
     	render 	:status => 200,
@@ -18,10 +24,15 @@ class MailboxsController < ApplicationController
   end
 
   def sent
-    @mail_box = current_user.mailbox.sentbox
+    current_user = User.find_by_authentication_token(params[:token])
+    @mail_box = current_user.mailbox.sentbox if current_user.present?
+    
     if @mail_box.present?
 	    sentbox = @mail_box.map do |mail|
-	    	{:id=>mail.id, :created_at => mail.created_at, :is_unread=> is_unread? mail.receipts, :sender=> mail.last_message.sender.name}
+				id = mail.id
+	    	created_at = mail.created_at
+	    	sender = mail.last_message.sender.name 
+	    	{ :id=>id, :created_at => created_at, :is_unread=> is_unread }
 	    end
 	    json = sentbox.to_json 
 	    
@@ -36,11 +47,16 @@ class MailboxsController < ApplicationController
   end
 
   def trash
-  	@mail_box = current_user.mailbox.trash
+  	current_user = User.find_by_authentication_token(params[:token])
+    @mail_box = current_user.mailbox.trash if current_user.present?
+    
 	  
 	  if @mail_box.present?
 	    trashbox = @mail_box.map do |mail|
-	    	{:id=>mail.id, :created_at => mail.created_at, :is_unread=> is_unread? mail.receipts, :sender=> mail.last_message.sender.name}
+				id = mail.id
+	    	created_at = mail.created_at
+	    	sender = mail.last_message.sender.name 
+	    	{:id=>id, :created_at => created_at, :sender=> sender}
 	    end
 	    json = trashbox.to_json 
     
@@ -51,10 +67,10 @@ class MailboxsController < ApplicationController
     end
   end
 
-  def is_unread? receipts
-  	receipts.each do |recept|
-  		return true if recept.is_unread?
-  	end
-  	return false
-  end
+	def unread? inbox_mail
+    inbox_mail.receipts.inbox.each do |rm|
+      return true if rm.is_unread? and rm.message.sender != current_user
+    end
+    return false
+  end 
 end
